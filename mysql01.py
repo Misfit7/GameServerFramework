3c118c411bbd9c80eed945e2e094371b6e2a848d
Misfit7 <z842514363@gmail.com>
2022-11-08 16:09:16 +0800

    2022年11月8日

--- mysql01.py
+++ mysql01.py
@@ -20,25 +20,28 @@ cursor = conn_obj.cursor()  # 括号内不写参数,数据是元组套元组
 
 pMgr=PlayerMgr.PlayerMgr()
 p=pMgr.NewPlayer("张三", 1)
-ps=pickle.dumps(p)
 s1="insert into player(pname) values(%s)"
 s2="insert into playercontent(id, playercontent) values(%s, %s)"
 
 try:    
     cursor.execute(s1,p.uname)
     p.id=int(cursor.lastrowid)  #
+    ps=pickle.dumps(p)
     cursor.execute(s2,(p.id,ps))  #
     conn_obj.commit()
 except Exception as ex:
     conn_obj.rollback()
 
-sql1 = 'select * from player;'  # SQL语句会被高亮显示
+sql1 = 'select * from playerContent;'  # SQL语句会被高亮显示
 # 执行SQL语句
 affect_rows = cursor.execute(sql1)
 print(affect_rows)  # 执行SQL语句之后受影响的行数
 # 获取数据
 # res = cursor.fetchall()  # 获取所有数据
 res = cursor.fetchone()  # 获取一条数据
+px=res[1]
+p=pickle.loads(px)
+print(p)
 # res = cursor.fetchmany(2)  # 获取指定个数数据
 # cursor.scroll(1, 'relative')  # 相对于当前位置往后移动一个单位
 # res = cursor.fetchall()  # 获取所有数据
